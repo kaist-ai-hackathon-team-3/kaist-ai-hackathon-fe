6a60d5ea1b98e77cb349aaccd62d5bd36160126d
seohyj <linajoo03@gmail.com>
2024-07-31 02:35:58 +0900

    Feat: Kakao Login AndroidManifest.xml 수정 (android>app>src>main>AndroidManifest.xml)

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'login.dart';
-
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
 
 void main() {
+  KakaoSdk.init(
+        nativeAppKey: '845483f619ce7edf43af42ce8215559b',
+        //javaScriptAppKey: '${YOUR_JAVASCRIPT_APP_KEY}',
+    );
   runApp(MyApp());
 }
 
