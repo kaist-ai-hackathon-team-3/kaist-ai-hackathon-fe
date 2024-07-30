5d006d2fdfcea00f2eb6be8738e0a7d2970f7243
jaehee831 <jhlee.ug@gm.gist.ac.kr>
2024-07-30 21:19:44 +0900

    정책 detail, 로그인 연결 flow

--- lib/home.dart
+++ lib/home.dart
@@ -1,3 +1,4 @@
+import 'package:ai/policy_detail.dart';
 import 'package:flutter/material.dart';
 import 'home_category.dart';
 
@@ -87,11 +88,11 @@ class HomePage extends StatelessWidget {
                 child: ListView(
                   scrollDirection: Axis.horizontal,
                   children: [
-                    policyCard('소상공인 정책 지원', '2024.09~2024.11', '교육부',
+                    policyCard(context,'소상공인 정책 지원', '2024.09~2024.11', '교육부',
                         'https://via.placeholder.com/192x108'),
-                    policyCard('Crypto Art NFT', '6,510 Members', '교육부',
+                    policyCard(context,'Crypto Art NFT', '6,510 Members', '교육부',
                         'https://via.placeholder.com/161x108'),
-                    policyCard('Lord of the Rings', '1,050 Members', 'Movies',
+                    policyCard(context,'Lord of the Rings', '1,050 Members', 'Movies',
                         'https://via.placeholder.com/267x108'),
                   ],
                 ),
@@ -114,11 +115,11 @@ class HomePage extends StatelessWidget {
                 child: ListView(
                   scrollDirection: Axis.horizontal,
                   children: [
-                    policyCard('정책 이름', '기간', '교육부',
+                    policyCard(context,'정책 이름', '기간', '교육부',
                         'https://via.placeholder.com/150x108'),
-                    policyCard('Crypto Insiders', '4,412 Members', '교육부',
+                    policyCard(context,'Crypto Insiders', '4,412 Members', '교육부',
                         'https://via.placeholder.com/162x108'),
-                    policyCard('World News', '2,147 Members', 'News',
+                    policyCard(context,'World News', '2,147 Members', 'News',
                         'https://via.placeholder.com/267x108'),
                   ],
                 ),
@@ -147,58 +148,49 @@ class HomePage extends StatelessWidget {
     );
   }
 
-  Widget policyCard(
+  Widget policyCard(BuildContext context, 
       String title, String subtitle, String tag, String imageUrl) {
-    return Container(
-      width: 147,
-      margin: const EdgeInsets.only(right: 16.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            width: 147,
-            height: 108,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(11),
-              image: DecorationImage(
-                image: NetworkImage(imageUrl),
-                fit: BoxFit.cover,
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => PolicyDetailScreen(title: title),
+          ),
+        );
+      },
+      child: Container(
+        width: 147,
+        margin: const EdgeInsets.only(right: 16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              width: 147,
+              height: 108,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(11),
+                image: DecorationImage(
+                  image: NetworkImage(imageUrl),
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
-          ),
-          const SizedBox(height: 5),
-          Text(
-            title,
-            style: const TextStyle(
-              color: Color(0xFF303030),
-              fontSize: 14,
-              fontFamily: 'Poppins',
-              fontWeight: FontWeight.w400,
-              height: 1.5,
-              letterSpacing: 0.39,
+            const SizedBox(height: 5),
+            Text(
+              title,
+              style: const TextStyle(
+                color: Color(0xFF303030),
+                fontSize: 14,
+                fontFamily: 'Poppins',
+                fontWeight: FontWeight.w400,
+                height: 1.5,
+                letterSpacing: 0.39,
+              ),
             ),
-          ),
-          const SizedBox(height: 3),
-          Text(
-            subtitle,
-            style: const TextStyle(
-              color: Color(0xFF686A8A),
-              fontSize: 12,
-              fontFamily: 'Poppins',
-              fontWeight: FontWeight.w400,
-              height: 1.5,
-              letterSpacing: 0.34,
-            ),
-          ),
-          const SizedBox(height: 3),
-          Container(
-            padding: const EdgeInsets.symmetric(vertical: 3.0, horizontal: 8.0),
-            decoration: BoxDecoration(
-              color: Colors.grey[200],
-              borderRadius: BorderRadius.circular(11),
-            ),
-            child: Text(
-              tag,
+            const SizedBox(height: 3),
+            Text(
+              subtitle,
               style: const TextStyle(
                 color: Color(0xFF686A8A),
                 fontSize: 12,
@@ -207,10 +199,30 @@ class HomePage extends StatelessWidget {
                 height: 1.5,
                 letterSpacing: 0.34,
               ),
-              textAlign: TextAlign.center,
             ),
-          ),
-        ],
+            const SizedBox(height: 3),
+            Container(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 3.0, horizontal: 8.0),
+              decoration: BoxDecoration(
+                color: Colors.grey[200],
+                borderRadius: BorderRadius.circular(11),
+              ),
+              child: Text(
+                tag,
+                style: const TextStyle(
+                  color: Color(0xFF686A8A),
+                  fontSize: 12,
+                  fontFamily: 'Poppins',
+                  fontWeight: FontWeight.w400,
+                  height: 1.5,
+                  letterSpacing: 0.34,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/login.dart
+++ lib/login.dart
@@ -1,3 +1,4 @@
+import 'package:ai/home_category.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatelessWidget {
@@ -72,52 +73,62 @@ class LoginScreen extends StatelessWidget {
 
                 const SizedBox(height: 20),
                 // Log In Button
-                Container(
-                  width: 327,
-                  height: 58,
-                  decoration: ShapeDecoration(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30),
-                    ),
-                  ),
-                  child: Stack(
-                    children: [
-                      Positioned(
-                        left: 0,
-                        top: 0,
-                        child: Container(
-                          width: 327,
-                          height: 58,
-                          decoration: ShapeDecoration(
-                            color: const Color(0xFFDFF8AC),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(20),
-                            ),
-                          ),
-                        ),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const HomeCategoryScreen(),
                       ),
-                      const Positioned(
-                        left: 0,
-                        top: 0,
-                        child: SizedBox(
-                          width: 327,
-                          height: 58,
-                          child: Center(
-                            child: Text(
-                              'Log in',
-                              textAlign: TextAlign.center,
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 14,
-                                fontFamily: 'Poppins',
-                                fontWeight: FontWeight.w600,
-                                height: 0,
+                    );
+                  },
+                  child: Container(
+                    width: 327,
+                    height: 58,
+                    decoration: ShapeDecoration(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                    ),
+                    child: Stack(
+                      children: [
+                        Positioned(
+                          left: 0,
+                          top: 0,
+                          child: Container(
+                            width: 327,
+                            height: 58,
+                            decoration: ShapeDecoration(
+                              color: const Color(0xFFDFF8AC),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20),
                               ),
                             ),
                           ),
                         ),
-                      ),
-                    ],
+                        const Positioned(
+                          left: 0,
+                          top: 0,
+                          child: SizedBox(
+                            width: 327,
+                            height: 58,
+                            child: Center(
+                              child: Text(
+                                'Log in',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 14,
+                                  fontFamily: 'Poppins',
+                                  fontWeight: FontWeight.w600,
+                                  height: 0,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ],
