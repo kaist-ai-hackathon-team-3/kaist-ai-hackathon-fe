85205f7a84982c4cc2d01029619ab127ff5f7a6c
jaehee831 <jhlee.ug@gm.gist.ac.kr>
2024-07-31 19:10:48 +0900

    로그인에서 유저 정보 불러오기

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'user_provider.dart';
 import 'login.dart';
-
 
 void main() {
   runApp(MyApp());
@@ -9,9 +10,14 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: LoginScreen(),
-      debugShowCheckedModeBanner: false,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => UserProvider()),
+      ],
+      child: MaterialApp(
+        home: LoginScreen(),
+        debugShowCheckedModeBanner: false,
+      ),
     );
   }
 }
